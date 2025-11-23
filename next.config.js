7b254cbad7e9ef1f6f6bb16065a4f45d78b45a49
makej7403-bit <makej7403@gmail.com>
2025-11-23 21:15:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true,
+  experimental: {},
 };
 
 module.exports = nextConfig;
